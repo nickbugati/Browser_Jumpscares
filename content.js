f940afe8651e8578e6913d3c07dd7972a926b807
nickbugati <nicholasgongee@aol.com>
2023-10-24 15:49:28 -0400

    Initial commit

---
+++ content.js
@@ -0,0 +1,29 @@
+if (confirm("This extension will display jump scares with loud audio. Do you wish to proceed?")) {
+    setTimeout(() => {
+        const jumpScares = [
+            { imgSrc: "path_to_your_image1.jpg", audioSrc: "path_to_your_audio1.mp3" },
+            { imgSrc: "path_to_your_image2.jpg", audioSrc: "path_to_your_audio2.mp3" },
+            // Add more jump scare scenarios as needed
+        ];
+
+        const randomIndex = Math.floor(Math.random() * jumpScares.length);
+        const selectedJumpScare = jumpScares[randomIndex];
+
+        const jumpScareContainer = document.createElement('div');
+        jumpScareContainer.innerHTML = `
+        <img src="${selectedJumpScare.imgSrc}" alt="Jump Scare" />
+        <audio autoplay>
+          <source src="${selectedJumpScare.audioSrc}" type="audio/mpeg">
+          Your browser does not support the audio tag.
+        </audio>
+      `;
+        jumpScareContainer.style.cssText = `
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 10000;
+      `;
+        document.body.appendChild(jumpScareContainer);
+    }, Math.random() * 10000); // Adjust timing as needed
+}  
